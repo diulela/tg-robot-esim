3fda88b9607a3998d1333947a22977872fa9889d
test <test@github.com>
2025-11-05 20:24:56 +0800

    USDT充值

--- miniapp/src/services/api/index.ts
+++ miniapp/src/services/api/index.ts
@@ -1,11 +1,20 @@
-// API 服务统一导出
-export { ApiClient, apiClient, type ApiClientConfig } from './client'
-export { ProductApi, productApi } from './product'
-export { OrderApi, orderApi } from './order'
-export { WalletApi, walletApi, type USDTRechargeOrder, type RechargeHistoryResponse } from './wallet'
-export { RegionApi, regionApi } from './region'
-export { UserApi, userApi, type UserProfile, type UserProfileUpdate } from './user'
-export { SystemApi, systemApi, type HealthCheckResponse, type SystemConfigResponse } from './system'
+// 导入所有 API 实例
+import { ApiClient, apiClient, type ApiClientConfig } from './client'
+import { ProductApi, productApi } from './product'
+import { OrderApi, orderApi } from './order'
+import { WalletApi, walletApi, type USDTRechargeOrder, type RechargeHistoryResponse } from './wallet'
+import { RegionApi, regionApi } from './region'
+import { UserApi, userApi, type UserProfile, type UserProfileUpdate } from './user'
+import { SystemApi, systemApi, type HealthCheckResponse, type SystemConfigResponse } from './system'
+
+// 重新导出所有模块
+export { ApiClient, apiClient, type ApiClientConfig }
+export { ProductApi, productApi }
+export { OrderApi, orderApi }
+export { WalletApi, walletApi, type USDTRechargeOrder, type RechargeHistoryResponse }
+export { RegionApi, regionApi }
+export { UserApi, userApi, type UserProfile, type UserProfileUpdate }
+export { SystemApi, systemApi, type HealthCheckResponse, type SystemConfigResponse }
 
 // 统一的 API 对象，保持向后兼容
 export default {
